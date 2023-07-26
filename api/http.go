2cd56edc2ddeff4ac81f564a7e78410c8cd06aac
Nathan13888 <29968201+Nathan13888@users.noreply.github.com>
2023-07-26 06:54:49 +0000

    api: updated http client
    Signed-off-by: Nathan13888 <29968201+Nathan13888@users.noreply.github.com>

--- api/http.go
+++ api/http.go
@@ -4,54 +4,52 @@ import (
 	"bytes"
 	"io/ioutil"
 	"net/http"
+	"time"
 )
 
 // TODO: make parallized HTTP workers
 // TODO: auto time request and response length
 
 type PostRequest struct {
-	Url     string
-	Data    []byte
-	Channel chan<- []byte
+	Url  string
+	Data []byte
 }
 
-const HTTP_TIMEOUT = 350
+// const HTTP_TIMEOUT = 350
+const HTTP_TIMEOUT = 2000
 
-func (job PostRequest) PostJSON() error {
+func (job PostRequest) PostJSON() ([]byte, error) {
+	var resBody []byte
+
 	// io.Reader as body
 	body := bytes.NewBuffer(job.Data)
 
 	// set up request
 	req, err := http.NewRequest("POST", job.Url, body)
 	if err != nil {
-		return err
+		return resBody, err
 	}
 
 	req.Header.Set("Content-Type", "application/json")
 
 	// create http client
 	client := &http.Client{
-		// Timeout: time.Duration(HTTP_TIMEOUT) * time.Millisecond,
+		Timeout: time.Duration(HTTP_TIMEOUT) * time.Millisecond,
 	}
 
 	// send request with body
 	res, err := client.Do(req)
 	if err != nil {
-		return err
+		return resBody, err
 	}
 
 	// parse response body
 	defer res.Body.Close()
 
-	resBody, err := ioutil.ReadAll(res.Body)
+	resBody, err = ioutil.ReadAll(res.Body)
 	if err != nil {
-		return err
+		return resBody, err
 	}
 
-	// send received body to channel
-	if job.Channel != nil {
-		job.Channel <- resBody
-	}
-
-	return nil
+	return resBody, nil
 }
