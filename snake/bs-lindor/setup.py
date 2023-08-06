1e6458dece189754fd9bd7502137459a6d163f80
Nathan13888 <29968201+Nathan13888@users.noreply.github.com>
2023-08-06 21:55:18 +0000

    snake: complete bs-lindor module
    Signed-off-by: Nathan13888 <29968201+Nathan13888@users.noreply.github.com>

---
+++ snake/bs-lindor/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="bs-lindor",
+    version="0.2.0",
+    description="",
+    long_description="",
+    long_description_content_type="text/markdown",
+    packages=["bs_lindor"],
+    zip_safe=False,
+)
